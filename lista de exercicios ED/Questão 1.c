e34ee4f725136bb8c39a83e58ce625629c4524f0
moonchilds2 <rennissteam@gmail.com>
2022-04-29 00:18:41 -0300

    Update Questão 1.c
    Utilizando lista encadeada de forma diferente e adicionando outras funcoes.

--- lista de exercicios ED/Questão 1.c
+++ lista de exercicios ED/Questão 1.c
@@ -16,7 +16,7 @@ conta* inicio = NULL;
 conta* fim = NULL;
 int tamanho = 0;
 
-void add(char* nome, int numero, int senha, double saldo) {
+void cadastrarConta(char* nome, int numero, int senha, double saldo) {
     
     conta* nova = malloc(sizeof(conta));
     nova->nome = nome;
@@ -39,8 +39,152 @@ void add(char* nome, int numero, int senha, double saldo) {
     }
 
     else if(nova->saldo >= fim->saldo) {
-        
+        nova->ant = fim;
+        fim->prox = nova;
+        fim = nova;
+    }
+
+    else{
+        conta* aux = inicio;
+
+        while(aux->prox->saldo < nova->saldo) {
+            aux = aux->prox;
+        }
+        nova->ant = aux;
+        nova->prox = aux->prox;
+        nova->ant->prox = nova;
+        nova->prox->ant = nova;
+
     }
 
     tamanho++;  
+}
+
+char* removerConta(int numero) {
+    conta* lixo;
+
+    if(numero == inicio->numero) {
+        lixo = inicio;
+        inicio = inicio->prox;
+    }
+    else if(numero == fim->numero){
+        lixo = fim;
+        fim = fim->ant;
+        fim->prox = NULL;
+    }
+    else {
+        conta* aux = inicio;
+
+        while(aux->numero != numero) {
+            aux = aux->prox;
+        }
+        lixo = aux;
+        aux->prox->ant = aux->ant;
+        aux->ant->prox = aux->prox;
+    }
+    char* titular_removido = lixo->nome;
+    free(lixo);
+    tamanho--;
+    
+    return titular_removido;
+}
+
+void depositar(int numero, int senha, int valor) {
+
+    if(numero == inicio->numero) {
+        if(senha == inicio->senha) {
+            inicio->saldo += valor;
+        }else{
+            printf("Senha incorreta.\n");
+        }
+    }
+
+    else if(numero == fim->numero) {
+        if(senha == fim->senha) {
+            fim->saldo += valor;
+        }else{
+            printf("Senha incorreta.\n");
+        }
+    }
+
+    else {
+        conta* aux = inicio;
+
+        while(numero != aux->numero) {
+            aux = aux->prox;
+        }
+
+        if(senha == aux->senha) {
+            aux->saldo += valor;
+        }else {
+            printf("Senha incorreta.\n");
+        }
+    }
+
+}
+
+void sacar(int numero, int senha, int valor) {
+    if(numero == inicio->numero) {
+        if(senha == inicio->senha) {
+            inicio->saldo -= valor;
+        }else{
+            printf("Senha incorreta.\n");
+        }
+    }
+
+    else if(numero == fim->numero) {
+        if(senha == fim->senha) {
+            fim->saldo -= valor;
+        }else{
+            printf("Senha incorreta.\n");
+        }
+    }
+
+    else {
+        conta* aux = inicio;
+
+        while(numero != aux->numero) {
+            aux = aux->prox;
+        }
+
+        if(senha == aux->senha) {
+            aux->saldo -= valor;
+        }else {
+            printf("Senha incorreta.\n");
+        }
+    }
+}
+
+void visualizar() {
+    conta* aux = inicio;
+
+    for(int i = 0; i < tamanho; i++) {
+        printf("\nTitular: %s, Saldo: %.2f\n\n", aux->nome, aux->saldo);
+        aux = aux->prox;
+    }
+}
+
+int main() {
+    cadastrarConta("Rodrigo", 1, 123, 20.00);
+    cadastrarConta("Fernando", 2, 123, 30.00);
+    cadastrarConta("Julia", 3, 123, 40.00);
+    cadastrarConta("Rennis", 4, 123, 50.00);
+    visualizar();
+
+    printf("Removendo uma conta");
+    sleep(1);
+    printf(".");
+    sleep(1);
+    printf(".");
+    sleep(1);
+    printf(".\n\n");
+    printf("Titular da conta removida: %s\n", removerConta(2));
+
+    printf("----------------------------\n\n");
+    depositar(4, 123, 50.00);
+    visualizar();
+    printf("----------------------------\n\n");
+    sacar(4, 123, 20.00);
+    visualizar();
+
 }
